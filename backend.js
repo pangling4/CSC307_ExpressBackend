9ce72e83e1eb5b5416873b0cbebd27927767f9fa
pangling4 <philipang515@gmail.com>
2022-04-06 13:11:58 -0700

    implement CORS

--- backend.js
+++ backend.js
@@ -1,38 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 const users = { 
     users_list :
-    [
-       { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
-       },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yat999', 
-          name: 'Dee',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       }
-    ]
+    []
  } 
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
